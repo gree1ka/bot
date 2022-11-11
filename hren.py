492ead5558c327363a1d7a659d5aca10e2267e70
gree1ka <sasha.vlasov.312@gmail.com>
2022-11-12 01:10:58 +0300

    Update hren.py

--- hren.py
+++ hren.py
@@ -1,8 +1,9 @@
 import requests
 import telebot
 from bs4 import BeautifulSoup
-import datetime
 from telebot import types
+from pendulum import now
+
 
 bot = telebot.TeleBot('5251329150:AAGUeWVWgRYEpbXI2GO7hLyfIk_GGUvpNUg')
 
@@ -12,7 +13,7 @@ def bitcoin():
     return(requests.get(url=url).json()['data']['last'])
 
 
-def get_text(month = datetime.date.today().month   , day = datetime.date.today().day):
+def get_text(month = now().month , day = now().day): #Делаем так что бы ставился понедельник
     url = f'https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya?group=54963&date=2022-{int(month)}-{int(day)}' #страница расписания нашей группы с нынешней датой
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36' #что бы удалось спарсить, иначе за бота принимает система
     code = requests.get(url, headers={'User-Agent': user_agent}) #получаем код сайта
@@ -20,7 +21,7 @@ def get_text(month = datetime.date.today().month   , day = datetime.date.today()
     return(soup)
 
 
-def get_raspisanie_from_text(soup, weekday = int(datetime.date.today().weekday()) + 1):
+def get_raspisanie_from_text(soup, weekday = int(now().weekday() + 1)):
     date_massive = []
     raspisanie_massive = []
     vremya_par = ['1\n9:00 - 10:35\n', '2\n10:45 - 12:20\n', '3\n13:00 - 14:35\n', '4\n14:45 - 16:20\n', '5\n16:30 - 18:05\n', '6\n18:15 - 19:50\n']
@@ -42,45 +43,15 @@ def get_raspisanie_from_text(soup, weekday = int(datetime.date.today().weekday()
     return(res)
 
 
-@bot.message_handler(commands=['rasp'])
-def rasp(message):
-    day = (message.text.split())[1]
-    month = (message.text.split())[3]
-    weekday = (message.text.split())[2]
-    date_massive = []
-    raspisanie_massive = []
-    vremya_par = ['1\n9:00 - 10:35\n', '2\n10:45 - 12:20\n', '3\n13:00 - 14:35\n', '4\n14:45 - 16:20\n', '5\n16:30 - 18:05\n', '6\n18:15 - 19:50\n'] #это что бы время пар не парсить, просто отедльно
-
-    url = f'https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya?group=54963&date=2022-{int(month)}-{int(day)}' #страница расписания нашей группы с нынешней датой
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36' #что бы удалось спарсить, иначе за бота принимает система
-    code = requests.get(url, headers={'User-Agent': user_agent}) #получаем код сайта
-    soup = BeautifulSoup(code.text)     #создаем переменную с которой будем искать
-    tables=soup.find_all('div', {'class': f'vt239 rasp-day rasp-day{int(weekday)}'})
-    for ind, n in enumerate(tables):
-        n = n.text.strip()    #нужно что бы получить чисто текст, без всяких дивов и тд
-        if n != '':
-            n = list(map(lambda x: x.strip(), n.split('\n')))
-            while '' in n:
-                n.remove('')
-            n = '\n'.join(n)
-            date_massive.append(vremya_par[ind])
-            raspisanie_massive.append(n)
-    res = str()
-    for i in range(len(date_massive)):
-        res += date_massive[i] + raspisanie_massive[i] + '\n\n'
-    if res == '':
-        bot.send_message(message.chat.id, 'Занятий нет, идём пить пиво!\nhttps://krasnoeibeloe.ru/\nhttps://yandex.ru/maps?rtext=59.902821%2C30.489175~60.049597%2C30.426361&rtt=mt')
-        
-        return
-    bot.send_message(message.chat.id, res)
-
-
 @bot.message_handler(commands=['start'])
 def start(m, res=False):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Расписание на неделю")
-    btn2 = types.KeyboardButton("Расписание на сегодня")
-    markup.add(btn1, btn2)
+    btn1 = types.KeyboardButton("Эта неделя")
+    btn2 = types.KeyboardButton("Сегодня")
+    btn3 = types.KeyboardButton('Следующая неделя')
+    btn4 = types.KeyboardButton('Своя дата')
+    btn5 = types.KeyboardButton('Курс Bitcion')
+    markup.add(btn1, btn2, btn3, btn4, btn5)
     bot.send_message(m.chat.id, 'Я работаю, напиши что - нибудь...', reply_markup=markup)
 
 
@@ -89,23 +60,29 @@ def btc(message):
     bot.send_message(message.chat.id, bitcoin())
 
 
-@bot.message_handler(commands=['week'])
-def week(message):
+def week(message, needed_date=now().add(days=-now().weekday()).date()):
     week_days = ['*Понедельник*', '*Вторник*', '*Среда*', '*Четверг*', '*Пятница*', '*Суббота*']
-    text = get_text()
+    text = get_text(needed_date.month, needed_date.day)
     for i in range(1, 7):
         res = get_raspisanie_from_text(text, i)
         if 'Занятий нет' not in res and 'Военная подготовка' not in res:
-            bot.send_message(message.chat.id, week_days[i-1] + '\n\n' + res, parse_mode='MarkDown')
+            bot.send_message(message.chat.id, week_days[i-1] + f'* - {needed_date.add(days=i-1).month:02}.{needed_date.add(days=i-1).day:02}*' + '\n\n' + res, parse_mode='MarkDown')
     return
 
 
 @bot.message_handler(content_types=['text'])
 def knopki(message):
-        if message.text == 'Расписание на неделю':
+        if message.text == 'Эта неделя':
             week(message)
-        elif message.text == 'Расписание на сегодня':
+        elif message.text == 'Сегодня':
             bot.send_message(message.chat.id, get_raspisanie_from_text(get_text()))
+        elif message.text == 'Следующая неделя':
+            week(message, now().add(days=7 - now().weekday()))  #Тот день окажется всегда понедельником
+        elif message.text == 'Курс Bitcion':
+            bot.send_message(message.chat.id, str(bitcoin()) + '$')
+        
+
+
 
 
 bot.polling(non_stop=True, interval=0)
